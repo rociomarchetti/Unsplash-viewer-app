c3e29a1be1a4c3cac448d24fc8d5c9092982fc0e
rociomarchetti <marchettirociob@gmail.com>
2023-03-14 12:20:51 +0100

    Updated visuals

--- src/app/images/main-page/main-page.component.ts
+++ src/app/images/main-page/main-page.component.ts
@@ -1,6 +1,6 @@
 import { Images } from './../interfaces/images.interface';
 import { UnsplashService } from './../service/unsplash.service';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
@@ -9,14 +9,47 @@ import { Component } from '@angular/core';
 })
 export class MainPageComponent {
   imagesArr: Images[] = [];
-  counter: number = 1;
-  isShown: boolean = false;
+  counter: number = 0;
+  isShown: boolean = true;
+  showMore: boolean = false;
 
   getPhotos() {
     this.apiService.callApi(this.counter).subscribe((resp) => {
       this.imagesArr = resp;
       this.counter++;
     });
+  }
+
+  openModal(index: number) {
+    const modal = document.getElementById(`myModal${index}`);
+    if (modal) {
+      modal.classList.add('show');
+      modal.style.display = 'block';
+    }
+  }
+
+  closeModal(index: number) {
+    const modal = document.getElementById(`myModal${index}`);
+    if (modal) {
+      modal.classList.remove('show');
+      modal.setAttribute('aria-hidden', 'true');
+      modal.setAttribute('style', 'display: none');
+    }
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const documentHeight = document.documentElement.scrollHeight;
+    const windowHeight = window.innerHeight;
+    const scrollPosition =
+      window.scrollY ||
+      window.pageYOffset ||
+      document.body.scrollTop +
+        ((document.documentElement && document.documentElement.scrollTop) || 0);
+
+    if (documentHeight - windowHeight - scrollPosition < 1) {
+      this.showMore = true;
+    }
   }
 
   showText() {
